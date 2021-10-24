9196e8dcd1892a45ac8f9725786c3eb5976a1208
David Folarin <dof5@cornell.edu>
2021-10-23 20:33:18 -0400

    fix bug due to dash in argument and black source cli.py

--- api_watchdog/cli.py
+++ api_watchdog/cli.py
@@ -6,10 +6,14 @@ from api_watchdog.collect import collect_results
 from api_watchdog.core import WatchdogTest
 from api_watchdog.runner import WatchdogRunner
 
+
 def topological_print(result_group, file=None):
     file = file or sys.stdout
-    for result in sorted(result_group.results, key=lambda g: g.test.name):
-        print(f"{result.test.name:<20} {'Pass' if result.success else 'Fail'} {result.latency:<12.3f}", file=file)
+    for result in sorted(result_group.results, key=lambda g: g.test_name):
+        print(
+            f"{result.test_name:<20} {'Pass' if result.success else 'Fail'} {result.latency:<12.3f}",
+            file=file,
+        )
     for group in sorted(result_group.groups, key=lambda g: g.name):
         topological_print(group, file=file)
 
@@ -17,7 +21,7 @@ def discover(args):
     runner = WatchdogRunner()
     tests = [
         WatchdogTest.parse_file(p)
-        for p in args.search_directory.rglob(args.pattern)
+        for p in vars(args)['search-directory'].rglob(args.pattern)
     ]
     results = runner.run_tests(tests)
     grouped_results = collect_results(results)
@@ -31,14 +35,30 @@ def cli():
     parser = ArgumentParser(description="API Watchdog CLI")
     subparsers = parser.add_subparsers()
 
-    parser_discover = subparsers.add_parser("discover", help="Discover and run tests in a directory")
-    parser_discover.add_argument("search_directory", type=Path, help="Directory to recursively search for tests")
-    parser_discover.add_argument("--pattern", "-p", type=str, default="*.watchdog.json", help="glob style pattern to match against for tests")
-    parser_discover.add_argument("--output_path", "-o", type=Path, default=None, help="Path to output test results file. When not provided, results are sent to stdout")
+    parser_discover = subparsers.add_parser(
+        "discover", help="Discover and run tests in a directory"
+    )
+    parser_discover.add_argument(
+        "search-directory",
+        type=Path,
+        help="Directory to recursively search for tests",
+    )
+    parser_discover.add_argument(
+        "--pattern",
+        "-p",
+        type=str,
+        default="*.watchdog.json",
+        help="glob style pattern to match against for tests",
+    )
+    parser_discover.add_argument(
+        "--output_path",
+        "-o",
+        type=Path,
+        default=None,
+        help="Path to output test results file. When not provided, results are"
+        " sent to stdout",
+    )
     parser_discover.set_defaults(func=discover)
 
     args = parser.parse_args()
     args.func(args)
-
-
-
